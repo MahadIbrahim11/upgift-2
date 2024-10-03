9d01cd8d7d949d7688b0063198e7091dca1b2650
maib07001 <maib07001@E-PW05C37V>
2024-10-03 14:32:10 +0200

    Add project files.

---
+++ upgift-2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Uppgift_2._1
+
+{
+    class program
+    {
+        static void Main(string[] arg)
+        {
+            Console.WriteLine("vilken favorit book har du");
+            string lucy = Console.ReadLine();
+            Console.WriteLine(lucy);
+
+        }
+
+    }
+
+}
